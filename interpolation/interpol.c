3b5be630d7e03c5edb61171869470844efa6fec4
whitestrat <kuk.lin.fed.or@yandex.ru>
2025-09-29 19:59:48 +0300

    create c file

---
+++ interpolation/interpol.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+float f(float x){
+    float sin = pow(sin(x/5), 2);
+    return 1/2*(exp(sin) - exp(-sin));
+}
+
+float* make_points(float (*f)(float), int N){
+    
+}
+
+float* interpolate_L(float **points, int N){
+    
+
+}
+
+float* interpolate_N(float **points, int N){
+
+
+}
+int main(){
+
+    float points[]
+    return 0;
+}
